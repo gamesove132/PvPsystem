481d6e74bcd6496e2c98ac86980ca7b05fc34e02
gamesove132 <gamesove132@gmail.com>
2026-05-01 18:53:38 +0300

    Update PvpManager.java

--- src/main/java/com/Pvpnew/Manager/PvpManager.java
+++ src/main/java/com/Pvpnew/Manager/PvpManager.java
@@ -1,6 +1,6 @@
 package com.Pvpnew.Manager;
 
-import com.pvpsystem.PvpPlugin;
+import com.Pvpnew.PvpPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
